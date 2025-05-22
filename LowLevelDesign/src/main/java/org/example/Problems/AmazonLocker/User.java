7c5d8949a9fe3ef0a30811b0b93ea59d958591d1
koushikshanker <koushikshanker75@gmail.com>
2025-05-22 23:03:36 +0530

    added fix for user

--- LowLevelDesign/src/main/java/org/example/Problems/AmazonLocker/User.java
+++ LowLevelDesign/src/main/java/org/example/Problems/AmazonLocker/User.java
@@ -1,9 +1,9 @@
 package org.example.Problems.AmazonLocker;
 
 public class User {
-    private int id;
-    private String name;
-    private String contact;
+    private final int id;
+    private final String name;
+    private final String contact;
 
     public User(int id, String name, String contact) {
         this.id = id;
@@ -21,5 +21,10 @@ public class User {
 
     public String getContact() {
         return contact;
+    }
+
+    @Override
+    public String toString() {
+        return "User{id=" + id + ", name='" + name + '\'' + ", contact='" + contact + '\'' + '}';
     }
 }
